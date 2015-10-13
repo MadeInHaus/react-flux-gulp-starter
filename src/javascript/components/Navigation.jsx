a8849741108316bea59af052f0cb21897c8f01f1
Steve Scavo <stevescavo@gmail.com>
2015-10-13 10:33:50 -0700

    Fix active state functionality

--- src/javascript/components/Navigation.jsx
+++ src/javascript/components/Navigation.jsx
@@ -1,19 +1,15 @@
 'use strict';
 
 import React, {PropTypes} from 'react';
-import {Link} from 'react-router';
+import {IndexLink, Link} from 'react-router';
 
 export default class Navigation extends React.Component{
     render () {
-        var isActive = this.context.history.isActive;
-        var homeClass = isActive('/') ? 'selected' : '';
-        var aboutClass = isActive('/about') ? 'selected' : ''
-        var deadClass = isActive('/sgfjhsdgfjhsdgfjsd') ? 'selected' : ''
         return (
             <ul className="navigation">
-                <li className={homeClass}><Link to='/' activeClassName="selected">Home</Link></li>
-                <li className={aboutClass}><Link to='/about' activeClassName="selected">About</Link></li>
-                <li className={deadClass}><Link to='/sgfjhsdgfjhsdgfjsd' activeClassName="selected">404</Link></li>
+                <li><IndexLink to="/" activeClassName="selected">Home</IndexLink></li>
+                <li><Link to='/about' activeClassName="selected">About</Link></li>
+                <li><Link to='/makeamericagreatagain' activeClassName="selected">404</Link></li>
             </ul>
         );
     }
--- src/javascript/components/Routes.jsx
+++ src/javascript/components/Routes.jsx
@@ -1,16 +1,16 @@
 'use strict';
 
 import React from 'react';
-import { Router, Route, Link } from 'react-router';
+import { Router, Route, IndexRoute, Link } from 'react-router';
 import Application from './Application';
 import Home from './Home';
 import About from './About';
 import NotFound from './NotFound';
 
 export default (
-    <Router component={Application}>
-        <Route path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="*" component={NotFound} />
+    <Router path="/" component={Application}>
+        <IndexRoute component={Home}/>
+        <Route path="about" component={About}/>
+        <Route path="*" component={NotFound}/>
     </Router>
 )
