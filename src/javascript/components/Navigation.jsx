420f1aed1cada8db184098e252a80fdcea6f2f67
Claus Wahlers <claus@codeazur.com.br>
2015-12-07 13:54:14 -0200

    de-trumpify 404 link, because yuck.

--- src/javascript/components/Navigation.jsx
+++ src/javascript/components/Navigation.jsx
@@ -7,7 +7,7 @@ export default class Navigation extends React.Component{
             <ul className="navigation">
                 <li><IndexLink to="/" activeClassName="selected">Home</IndexLink></li>
                 <li><Link to='/about' activeClassName="selected">About</Link></li>
-                <li><Link to='/makeamericagreatagain' activeClassName="selected">404</Link></li>
+                <li><Link to='/makejavascriptgreatagain' activeClassName="selected">404</Link></li>
             </ul>
         );
     }
