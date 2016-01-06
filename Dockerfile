d156dd084a5b865146721f6f27dfa85b293fcbdb
Sam Bernard <sambernard@gmail.com>
2016-01-06 13:04:24 -0800

    Update docker run script to support docker-machine, make sure docker-osx-dev gets shut down

--- Dockerfile
+++ Dockerfile
@@ -14,16 +14,16 @@ WORKDIR /app/user
 
 # Install node
 RUN curl -s https://s3pository.heroku.com/node/v$NODE_ENGINE/node-v$NODE_ENGINE-linux-x64.tar.gz | tar --strip-components=1 -xz -C /app/heroku/node
-
-# Upgrade npm
 RUN /app/heroku/node/bin/npm install npm -g
 
 # Export the node path in .profile.d
 RUN echo "export PATH=\"/app/heroku/node/bin:/app/user/node_modules/.bin:\$PATH\"" > /app/.profile.d/nodejs.sh
 
-COPY package.json /app/user/
+RUN /app/heroku/node/bin/npm set registry http://registry.npmjs.org/ && \
+    /app/heroku/node/bin/npm set strict-ssl false
+
+ADD package.json /app/user/
 ADD .docker/node_modules.tar.gz /app/user/
 RUN /app/heroku/node/bin/npm install
-COPY . /app/user/
-
+ADD . /app/user/
 RUN gulp production
--- dev.sh
+++ dev.sh
@@ -5,11 +5,14 @@ pids=""
 
 function ctrl_c() {
     echo "** Trapped CTRL-C"
-    docker-compose stop
+    docker-compose -f docker-compose.dev.yml stop || true
+
     if [ -n "$pids" ]
     then
         kill -- $pids
     fi
+
+    pkill -f docker-osx-dev
 
     exit $?
 }
@@ -30,8 +33,7 @@ function export_node_modules() {
     fi
 
     CURRENT_NPM_REVISION="$(git rev-list -1 HEAD package.json)"
-
-    PACKAGE_JSON_CHANGED="$(git status --porcelain | grep package.json)"
+    PACKAGE_JSON_CHANGED="$(git status --porcelain | grep package.json || echo '')"
 
     if [ "$CURRENT_NPM_REVISION" != "$CACHED_NPM_REVISION" ] || [ -n "$PACKAGE_JSON_CHANGED" ]; then
         rm -Rf .docker/node_modules
@@ -45,11 +47,37 @@ function export_node_modules() {
     fi
 }
 
-docker-osx-dev install
-boot2docker up
-eval "$(boot2docker shellinit)"
+USE_BOOT2DOCKER=true
 
+while [[ $# > 0 ]]
+do
+key="$1"
 
+case $key in
+    -d|--docker-machine)
+    USE_BOOT2DOCKER=false
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+shift # past argument or value
+done
+
+if [ "$USE_BOOT2DOCKER" = true ]; then
+    boot2docker up
+    eval "$(boot2docker shellinit)"
+
+else
+    docker-machine start default || true
+    eval "$(docker-machine env default)"
+fi
+
+# Skip Dependencies isn't supported in older versions
+docker-osx-dev install --skip-dependencies || docker-osx-dev install
+pkill -f docker-osx-dev || true
+
+docker-compose -f docker-compose.dev.yml stop || true
 
 # Make sure node_modules.tar.gz exists
 if [ ! -f ".docker/node_modules.tar.gz" ]; then
@@ -61,17 +89,13 @@ docker-compose -f docker-compose.dev.yml build
 
 trap ctrl_c SIGINT SIGTERM INT TERM
 
-docker events -f event=start | export_node_modules &
+docker-osx-dev -c docker-compose.dev.yml &
 pids="$pids $!"
 
-
-docker-osx-dev -c docker-compose.dev.yml &
-
+docker events -f event=start | grep web | export_node_modules &
 pids="$pids $!"
 
 docker-compose -f docker-compose.dev.yml up &
-
 pids="$pids $!"
 
-
-wait $pids
+wait $pids;
