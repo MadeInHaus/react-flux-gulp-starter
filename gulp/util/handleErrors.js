f3cc7e0146504a8acca551c991ba8ef863572741
Sam Bernard <sambernard@gmail.com>
2016-02-11 12:53:12 -0800

    Get rid of gulp-notify, call notify-send directly to fix invalid argument errors. closes #7

--- gulp/util/handleErrors.js
+++ gulp/util/handleErrors.js
@@ -1,15 +1,19 @@
-var notify = require("gulp-notify");
+var exec = require('child_process').exec;
 
-module.exports = function() {
+module.exports = function(err) {
 
   var args = Array.prototype.slice.call(arguments);
+  	var exec = require('child_process').exec;
 
-  // Send error to notification center with gulp-notify
-  notify.onError({
-    title: "Compile Error",
-    message: "<%= error %>"
-  }).apply(this, args);
+	exec(['eval $(notify-send "Compile Error" "', err.message , '")'].join(''),
+	  function (error, stdout, stderr){
+	    console.log(stdout);
+	    console.log(stderr);
+	    if (error !== null) {
+	      console.log(error);
+	    }
+	});  	
 
-  // Keep gulp from hanging on this task
+	// Keep gulp from hanging on this task
   this.emit('end');
 };
