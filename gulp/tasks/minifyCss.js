869fb3415fdeeff83745db890ae908ef7de2982e
Claus Wahlers <claus@codeazur.com.br>
2016-02-11 19:14:29 -0200

    #6: use `gulp-cssnano` instead of deprecated `gulp-minify-css`

--- gulp/tasks/minifyCss.js
+++ gulp/tasks/minifyCss.js
@@ -1,13 +1,11 @@
 var gulp = require('gulp-help')(require('gulp'));
 var config = require('../config').production;
-var minifyCSS = require('gulp-minify-css');
+var cssNano = require('gulp-cssnano');
 var size = require('gulp-filesize');
 
 gulp.task('minifyCss', false, ['sass'], function() {
     return gulp.src(config.cssSrc)
-        .pipe(minifyCSS({
-            keepBreaks: true
-        }))
+        .pipe(cssNano())
         .pipe(gulp.dest(config.dest + '/css'))
         .pipe(size());
 })
