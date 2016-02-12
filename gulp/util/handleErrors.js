30244fcda08724e887b4271135cb0ef8a4ad0861
Sam Bernard <sambernard@gmail.com>
2016-02-12 11:35:06 -0800

    Ensure compile errors get logged to console

--- gulp/util/handleErrors.js
+++ gulp/util/handleErrors.js
@@ -3,17 +3,20 @@ var exec = require('child_process').exec;
 module.exports = function(err) {
 
   var args = Array.prototype.slice.call(arguments);
-  	var exec = require('child_process').exec;
+    var exec = require('child_process').exec;
 
-	exec(['eval $(notify-send "Compile Error" "', err.message , '")'].join(''),
-	  function (error, stdout, stderr){
-	    console.log(stdout);
-	    console.log(stderr);
-	    if (error !== null) {
-	      console.log(error);
-	    }
-	});  	
 
-	// Keep gulp from hanging on this task
+    console.log(err.message);
+
+    exec(['eval $(notify-send "Compile Error" "', err.message , '")'].join(''),
+      function (error, stdout, stderr){
+        console.log(stdout);
+        console.log(stderr);
+        if (error !== null) {
+          console.log(error);
+        }
+    });
+
+    // Keep gulp from hanging on this task
   this.emit('end');
 };
