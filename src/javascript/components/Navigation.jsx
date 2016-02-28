900e8a7b1bd0e3c62c4bf0e5df11885ec17fa7ed
Steve Scavo <stevescavo@gmail.com>
2016-02-28 01:16:18 -0800

    Refactor SCSS approach
    - Import fonts first and foremost
    - Rename `base` partial to `root.scss`
    - "Layouts" partial is now responsible for importing specific layout components, as well as common layout classes, i.e. `.container`
    - Break out "links" from "typography" so it's more inline with the W3C category and spec
    - Build out a mobile-first UI (this was grating on my soul)
    - Prefer `em` over `rem` units
    - Cleanup, revise and prune comments

--- src/javascript/components/Navigation.jsx
+++ src/javascript/components/Navigation.jsx
@@ -5,11 +5,12 @@ export default class Navigation extends React.Component {
 
     render() {
         return (
-            <ul className="navigation">
-                <li><IndexLink to="/" activeClassName="selected">Home</IndexLink></li>
-                <li><Link to="/about" activeClassName="selected">About</Link></li>
-                <li><Link to="/makejavascriptgreatagain" activeClassName="selected">404</Link></li>
-            </ul>
+            <div className="container">
+                <IndexLink to="/" className="logo" activeClassName="selected">Home</IndexLink>
+                <Link to="/about" activeClassName="selected">About</Link>
+                <Link to="/makejavascriptgreatagain" activeClassName="selected">404</Link>
+                <Link to="https://github.com/MadeInHaus/react-flux-gulp-starter" target="_blank">Github</Link>
+            </div>
         );
     }
 
