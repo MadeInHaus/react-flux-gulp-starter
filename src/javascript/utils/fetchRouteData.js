afa0c4b49c3280ab1cb3e93dfeaa4840c6e2a800
Sam Bernard <sambernard@gmail.com>
2016-10-18 15:09:46 -0700

    Allow for routes that don't have a component

--- src/javascript/utils/fetchRouteData.js
+++ src/javascript/utils/fetchRouteData.js
@@ -4,7 +4,7 @@ import executeMultiple from 'fluxible-action-utils/async/executeMultiple';
 
 export default function fetchRouteData(context, routerState) {
     const actionsArray = routerState.components
-                        .filter(component => _.isFunction(component.load))
+                        .filter(component => component && _.isFunction(component.load))
                         .map(component => component.load(context, routerState.params))
                         .reduce((actions, componentActions) => (
                             _.assign(actions, componentActions)
