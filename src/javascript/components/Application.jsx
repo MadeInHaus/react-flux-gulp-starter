9732fbcda3c03c6a5e1fde6de2e4ea9675825cbb
Steve Scavo <stevescavo@gmail.com>
2017-08-07 17:49:38 -0700

    Import `PropTypes` from standalone package

--- src/javascript/components/Application.jsx
+++ src/javascript/components/Application.jsx
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Navigation from 'components/Navigation';
 import { connectToStores } from 'fluxible-addons-react';
 
